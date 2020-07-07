0724bace6e70190a7880cea2576cfac7d0b8c992
hebertonx <heberton.cdd@gmail.com>
2020-07-07 19:45:34 -0300

    Teste aula 13

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -2,4 +2,10 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 
+	public static void main(String[] args) {
+		System.out.println("Primeiro programa! Parte 01");
+		System.out.println("Primeiro programa! Parte 02");
+		System.out.println("Primeiro programa! Parte 03");
+	}
+
 }

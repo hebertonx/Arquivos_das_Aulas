0b683de70adfd5ba94264cb1dc4c78ac6abc4943
Heberton <heberton.cdd@gmail.com>
2020-07-14 22:29:41 -0300

    Aula 13

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,15 +1,28 @@
 package fundamentos;
-
+/**
+ * 	
+ * @author Nascimento
+ * @since JDK1.0
+ */
 public class PrimeiroPrograma {
 	
+	/**
+	 * 
+	 * @param args
+	 */
+		
 	public static void main(String[] args) {
-		System.out.println("Primeiro programa!");
-	}
-
-	public static void main(String[] args) {
+		//Uma sentenca de código termina com;
+		//Mais uma linha
+		//Fim
 		System.out.println("Primeiro programa! Parte 01");
+		
+		/*
+		 * Linha1
+		 * Linha2
+		 * Linha3
+		 */
 		System.out.println("Primeiro programa! Parte 02");
-		System.out.println("Primeiro programa! Parte 03");
-	}
+			}
 
 }
